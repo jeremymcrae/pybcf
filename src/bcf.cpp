08f782b9b9069adea784bb10e0d17a768ccdbf21
Jeremy McRae <jmcrae@illumina.com>
2024-03-28 12:58:00 -0700

    fixed error message

--- src/bcf.cpp
+++ src/bcf.cpp
@@ -10,7 +10,7 @@ namespace bcf {
 BCF::BCF(std::string path) {
   std::ifstream tmp(path, std::ios::in | std::ios::binary);
   if (!tmp.is_open()) {
-    throw std::invalid_argument("cannot open chain file at " + path);
+    throw std::invalid_argument("cannot open file at " + path);
   }
   igzstream infile(path.c_str());
   

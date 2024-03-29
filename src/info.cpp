f88814313b7c72e4d9ce72865ae90c2e4180fe66
Jeremy McRae <jmcrae@illumina.com>
2024-03-29 16:31:55 -0700

    improved info parsing

--- src/info.cpp
+++ src/info.cpp
@@ -6,6 +6,23 @@
 
 namespace bcf {
 
+std::int64_t get_int(igzstream & infile, std::uint8_t size) {
+  std::int64_t val=0;
+  infile.read(reinterpret_cast<char *>(&val), size);
+  return val;
+}
+
+float get_float(igzstream & infile) {
+  float val;
+  infile.read(reinterpret_cast<char *>(&val), 4);
+  return val;
+}
+
+std::string get_string(igzstream & infile, std::uint32_t size) {
+  std::string val(size, ' ');
+  infile.read(reinterpret_cast<char *>(&val[0]), size);
+  return val;
+}
 
 Info::Info(igzstream & infile, Header & header, std::uint32_t n_info) {
 
@@ -13,8 +30,14 @@ Info::Info(igzstream & infile, Header & header, std::uint32_t n_info) {
   Typed type_val;
 
   // read the info fields. TODO - find out a way to skip this if not required
-  std::uint32_t info_idx;
+  std::int8_t info_idx;
+  std::uint32_t idx;
   std::string key;
+
+  float f_val;
+  std::int32_t i_val;
+  std::string s_val;
+
   for (std::uint32_t i = 0; i < n_info; i++) {
     infile.read(reinterpret_cast<char *>(&typing), sizeof(std::uint8_t));
     type_val = {typing, infile};
@@ -23,11 +46,87 @@ Info::Info(igzstream & infile, Header & header, std::uint32_t n_info) {
 
     std::cout << "info key: " << key << std::endl;
 
-    // now parse value
+    // now parse the value
     infile.read(reinterpret_cast<char *>(&typing), sizeof(std::uint8_t));
     type_val = {typing, infile};
-    if (type_val.n_vals == 1)
-    {
+    
+    // figure out whuch datastore to keep values in
+    switch (type_val.type) {
+      case flag:
+        info_idx = -1;
+        idx = 0;
+        break;
+      case float_:
+        info_idx = 0;
+        break;
+      case char_:
+        info_idx = 2;
+        break;
+      default:
+        info_idx = 1;
+        break;
+    }
+    
+    if (type_val.type != flag) {
+      if (type_val.n_vals > 1) {
+        // increase offset for vector values
+        info_idx += 3;
+      }
+      if (type_val.type == float_) {
+        if (type_val.n_vals == 1) { 
+          idx = scalar_floats.size();
+        } else {
+          idx = vector_floats.size();
+          vector_floats.push_back({});
+        }
+      } else if (type_val.type == char_) {
+        if (type_val.n_vals == 1) {
+          idx = scalar_strings.size();
+        } else {
+          idx = vector_strings.size();
+          vector_strings.push_back({});
+        }
+      } else {
+        if (type_val.n_vals == 1) {
+          idx = scalar_ints.size();
+        } else {
+          idx = vector_ints.size();
+          vector_ints.push_back({});
+        }
+      }
+    }
+    
+    keys[key] = {info_idx, idx};
+    
+    for (std::uint32_t i=0; i < type_val.n_vals; i++) {
+      switch(type_val.type) {
+        case flag:
+          break;
+        case float_:
+          f_val = get_float(infile);
+          if (type_val.n_vals == 1) {
+            scalar_floats.push_back(f_val);
+          } else {
+            vector_floats[idx].push_back(f_val);
+          }
+          break;
+        case char_:
+          s_val = get_string(infile, type_val.n_vals);
+          if (type_val.n_vals == 1) {
+            scalar_strings.push_back(s_val);
+          } else {
+            vector_strings[idx].push_back(s_val);
+          }
+          break;
+        default:
+          i_val = get_int(infile, type_val.n_vals);
+          if (type_val.n_vals == 1) {
+            scalar_ints.push_back(i_val);
+          } else {
+            vector_ints[idx].push_back(i_val);
+          }
+          break;
+      }
     }
   }
 }
--- src/info.h
+++ src/info.h
@@ -4,15 +4,34 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "gzstream/gzstream.h"
 #include "header.h"
 
 namespace bcf {
 
+struct InfoType {
+  std::int8_t type;
+  std::uint32_t offset;
+};
+
 class Info {
+  // for each info record, track which type it is, and some index value
+  std::unordered_map<std::string, InfoType> keys;
 public:
   Info(igzstream & infile, Header & header, std::uint32_t n_info);
+  bool get(std::string key);
+
+  // keep a number of data stores, for the different value types, which can be
+  // one of "Integer, Float, Flag, Character, and String"
+  std::vector<float> scalar_floats;                      // type 0
+  std::vector<std::int64_t> scalar_ints;                 // type 1
+  std::vector<std::string> scalar_strings;               // type 2
+  
+  std::vector<std::vector<float>> vector_floats;         // type 3
+  std::vector<std::vector<std::int64_t>> vector_ints;    // type 4
+  std::vector<std::vector<std::string>> vector_strings;  // type 5
 };
 
 } // namespace

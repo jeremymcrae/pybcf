f95fea715d1764a602a185473698c35c2395858f
Jeremy McRae <jmcrae@illumina.com>
2024-04-02 11:01:44 -0700

    store n_samples in class, and better key parsing

--- src/format.cpp
+++ src/format.cpp
@@ -33,32 +33,30 @@ static std::string get_string(const char * buf, std::uint32_t & idx, std::uint32
   return val;
 }
 
-Format::Format(igzstream & infile, Header & _header, std::uint32_t len, std::uint32_t n_fmt) {
+Format::Format(igzstream & infile, Header & _header, std::uint32_t len, std::uint32_t n_fmt, std::uint32_t _n_samples) {
+  n_samples = _n_samples;
   header = &_header;
   if (len == 0) {
     return;
   }
   
-  std::cout << "reading format: " << len << std::endl;
   // read the sample data into a buffer, but don't parse until required
   buf = new char[len];
   infile.read(reinterpret_cast<char *>(&buf[0]), len);
   
-  // read the keys available
+  // read the available keys
   std::uint32_t buf_idx=0;
   std::uint32_t format_idx=0;
   std::string key;
   Typed type_val;
   for (std::uint32_t i = 0; i < n_fmt; i++ ){
     type_val = {buf, buf_idx};
-    std::cout << "format_type: " << type_val.type << ", size: " << type_val.type_size << ", len: " << type_val.n_vals << std::endl;
     format_idx = get_int(buf, buf_idx, type_val.type_size);
-    std::cout << "format_idx: " << format_idx << std::endl;
     key = header->format[format_idx].id;
-    std::cout << "key: " << key << std::endl;
 
     type_val = {buf, buf_idx};
     keys[key] = {type_val.type, type_val.type_size, buf_idx, type_val.n_vals};
+    buf_idx += (type_val.n_vals * type_val.type_size * n_samples);
   }
 }
 
--- src/format.h
+++ src/format.h
@@ -24,8 +24,9 @@ class Format {
   std::unordered_map<std::string, FormatType> keys;
   char * buf={};
   Header * header;
+  std::uint32_t n_samples=0;
 public:
-  Format(igzstream & infile, Header & _header, std::uint32_t len, std::uint32_t n_fmt);
+  Format(igzstream & infile, Header & _header, std::uint32_t len, std::uint32_t n_fmt, std::uint32_t _n_samples);
   Format() {};
   // ~Format() {
   //   delete[] buf;
